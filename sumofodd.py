29fb0f910b3eaaf5fd1fe24d5666df1c073d4e30
yamongphoo <59530075+yamongphoo@users.noreply.github.com>
2022-01-25 22:32:11 +0630

    Add files via upload
    #the program is un accomplished due to the overload execution though the output  reached out of the terminal

---
+++ sumofodd.py
@@ -0,0 +1,39 @@
+#multiple of 3, sum of all odd numbers from 1000 to 10000
+
+
+#minimum,maximum = 1000,10000
+
+total = 0
+
+for n in range(1000,10001,3):
+    
+    if (n%2 != 0 ):
+        
+        print("{0}".format(n))
+        
+        total = total +n
+        
+print (" sum of odd numbers from {0} to {1} = {2}".format(1000,10001,total)) 
+        
+        
+        
+        
+
+
+    
+   
+        
+    
+
+        
+
+
+        
+        
+        
+            
+            
+        
+              
+        
+        
